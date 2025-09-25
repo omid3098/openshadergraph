6308d0e4f3a47aaa9e442ef98d7fe56090693eb2
Omid Saadat <omid3098@gmail.com>
2025-09-25 16:33:20 +0200

    chore: update version to 0.2.0, set commit hash to d83cd78e, update build date, and mark as dirty build

--- src/version.ts
+++ src/version.ts
@@ -1,8 +1,8 @@
 // Auto-generated at build time by build.ts
-export const APP_VERSION = "0.3.4";
-export const APP_COMMIT = "";
-export const APP_BUILD_DATE = "2025-09-25T13:36:09.860Z";
-export const APP_DIRTY = false;
+export const APP_VERSION = "0.2.0";
+export const APP_COMMIT = "d83cd78e";
+export const APP_BUILD_DATE = "2025-09-25T14:26:37.075Z";
+export const APP_DIRTY = true;
 export const APP_DEPLOY = "Local";
 
 export type AppVersionInfo = {
